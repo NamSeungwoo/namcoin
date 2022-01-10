0eec355313b810749fba847d473700c8d2cf63ae
VicNam <nevyhero@naver.com>
2022-01-10 17:35:31 +0900

    Messages

---
+++ p2p/messages.go
@@ -0,0 +1,18 @@
+package p2p
+
+type MessageKind int
+
+const (
+	MessageNewestBlock MessageKind = iota
+	MessageAllBlocksRequest
+	MessageAllBlocksResponse
+)
+
+type Message struct {
+	Kind    MessageKind
+	Payload []byte
+}
+
+func sendNewestBlock(p *peer) {
+
+}
