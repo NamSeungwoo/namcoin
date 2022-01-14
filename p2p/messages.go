b8044bcedc01c7dfd2d91c9006bf44f4e6518d75
VicNam <nevyhero@naver.com>
2022-01-14 17:05:02 +0900

    friday

--- p2p/messages.go
+++ p2p/messages.go
@@ -1,5 +1,10 @@
 package p2p
 
+import (
+	"github.com/NamSeungwoo/namcoin/blockchain"
+	"github.com/NamSeungwoo/namcoin/utils"
+)
+
 type MessageKind int
 
 const (
@@ -13,6 +18,19 @@ type Message struct {
 	Payload []byte
 }
 
-func sendNewestBlock(p *peer) {
+func (m *Message) addPayload(p interface{}) {
 
+}
+
+func makeMessage(kind MessageKind, payload interface{}) []byte {
+	m := Message{
+		Kind: kind,
+	}
+}
+
+func sendNewestBlock(p *peer) {
+	b, err := blockchain.FindBlock(blockchain.Blockchain().NewestHash)
+	utils.HandleErr(err)
+	m := makeMessage(MessageNewestBlock, b)
+	p.inbox <- m
 }
